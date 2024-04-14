de7f66ff6d2a2009c75f9efcee838e7593baed8c
Andrés Arias <arias9306@gmail.com>
2024-04-14 10:13:45 -0500

    chore: add typeorm lib

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,11 +1,6 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import 'reflect-metadata';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
